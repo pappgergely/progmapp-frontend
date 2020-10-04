f5862db71b4dee3844cf4ce3db57048e32393652
BHedi89 <hedi.besenyei89@gmail.com>
2020-10-04 15:23:42 +0200

    try to solve heroku problem

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || '8080';
 
 const app = express();
 
-app.set('port', PORT);
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/progmapp-frontend'));
+app.use(express.static(__dirname + '/dist/ProgmaticApp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/progmapp-frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/ProgmaticApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
