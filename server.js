f76a6a03bcdbcdf5eaac11b1d097efdf2c70d649
Nyilasy Péter <nyilasypeter@gmail.com>
2020-10-05 22:49:23 +0200

    heroku trial

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/progmapp-frontend'));
+app.use(express.static('./dist/progmapp-frontend'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/progmapp-frontend/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/progmapp-frontend' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
