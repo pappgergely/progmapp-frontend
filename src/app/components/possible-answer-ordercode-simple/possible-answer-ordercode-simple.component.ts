a63c647800d96cbe37587fc3e9186030a4474d5c
Nyilasy Péter <nyilasypeter@gmail.com>
2020-10-13 23:09:00 +0200

    source code task can be solved by student, source code basic pretty print added

--- src/app/components/possible-answer-ordercode-simple/possible-answer-ordercode-simple.component.ts
+++ src/app/components/possible-answer-ordercode-simple/possible-answer-ordercode-simple.component.ts
@@ -36,6 +36,36 @@ export class PossibleAnswerOrdercodeSimpleComponent extends PossibleAnswerRespon
   drop(event: CdkDragDrop<string[]>): void {
     console.log(event.previousIndex);
     moveItemInArray(this.possibleAnswer.possibleAnswerValues, event.previousIndex, event.currentIndex);
+    this.prettyPrint();
+  }
+
+  prettyPrint(): void{
+    const rows = document.querySelectorAll('.source-code-line');
+    let spaces = 0;
+    for (const pov of this.possibleAnswer.possibleAnswerValues){
+      const act = document.getElementById(pov.id);
+      const actText = act.innerText;
+      console.log(actText);
+      if (this.hasBlockClose(actText)){
+        spaces--;
+        if (spaces < 0){
+          spaces = 0;
+        }
+      }
+      const styleString =  (spaces * 20) + 'px';
+      act.setAttribute('style', 'margin-left:' + styleString);
+      if (this.hasBlockOpen(actText)){
+        spaces++;
+      }
+    }
+  }
+
+  hasBlockOpen(str: string): boolean{
+    return str.indexOf('{') !== -1;
+  }
+
+  hasBlockClose(str: string): boolean{
+    return str.indexOf('}') !== -1;
   }
 
 }
